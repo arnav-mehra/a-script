89bcecf9bfcac65ad7199d17c85365b9944c8227
ArnavMeh <mehraarn000@gmail.com>
2024-01-11 01:57:00 -0500

    fixed a lot

--- index.scala
+++ index.scala
@@ -1,121 +1,118 @@
-// package index
+package index
 
-// import scala.util.parsing.combinator.JavaTokenParsers
-// import scala.collection.mutable.HashMap
+import scala.util.parsing.combinator.JavaTokenParsers
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.ArrayBuffer
 
-// import util.{Wrapper, BlockType, BlockNode, BlockTree}
-// import line.LineParser
+import util.{Data, BlockType, BlockNode, BlockTree}
+import line.LineParser
 
-// object ProgramParser extends JavaTokenParsers {
-//     val functions: HashMap[String, BlockTree] = HashMap();
+object ProgramParser extends JavaTokenParsers {
+    val functions: HashMap[String, BlockTree] = HashMap();
 
-//     def program:Parser[BlockTree] = rep(line) ^^ (lst => lst)
+    def program:Parser[BlockTree] = rep(line | block) ^^ (lst => lst.to(ArrayBuffer))
 
-//     def function_block:Parser[BlockNode] = "fn " ~ variable ~ block ^^ {
-//         case "fn "~v~b => {
-//             functions(v) = b
-//             BlockNode(BlockType.Fn)
-//         }
-//         case default => {
-//             println("wtf")
-//             BlockNode()
-//         }
-//     }
+    // BLOCKS
 
-//     def while_block:Parser[BlockNode] = "while " ~ statement ~ block ^^ {
-//         case "while "~s~b => {
-//             BlockNode(BlockType.While, s, b)
-//         }
-//         case default => {
-//             println("wtf")
-//             BlockNode()
-//         }
-//     }
+    def block:Parser[BlockNode] = 
+        function_block
+        | while_block
+        | if_block
 
-//     def if_block:Parser[BlockNode] = "if " ~ statement ~ block ^^ {
-//         case "if "~s~b => {
-//             BlockNode(BlockType.If, s, b)
-//         }
-//         case default => {
-//             println("wtf");
-//             BlockNode()
-//         }
-//     }
+    def function_block:Parser[BlockNode] = "fn " ~ variable ~ block_core ^^ {
+        case "fn "~v~b => functions(v) = b; BlockNode(BlockType.Fn)
+        case default   => println("wtf");   BlockNode()
+    }
 
-//     def block:Parser[BlockTree] = "{" ~ rep(line) ~ "}" ^^ {
-//         case "{"~lst~"}" => lst
-//         case default => List()
-//     }
+    def while_block:Parser[BlockNode] = "while " ~ statement ~ block_core ^^ {
+        case "while "~s~b => BlockNode(BlockType.While, s, b)
+        case default      => println("wtf"); BlockNode()
+    }
 
-//     def line:Parser[BlockNode | String] =
-//         statement ~ ";" ^^ {
-//             case s~";" => s
-//         }
-//         | if_block
-//         | while_block
-//         | function_block
+    def if_block:Parser[BlockNode] = "if " ~ statement ~ block_core ^^ {
+        case "if "~s~b => BlockNode(BlockType.If, s, b)
+        case default   => println("wtf"); BlockNode()
+    }
 
-//     def statement:Parser[String] = "[^;{}]*".r ^^ (s => s)
+    def block_core:Parser[BlockTree] = "{" ~ program ~ "}" ^^ {
+        case "{"~lst~"}" => lst
+        case default     => ArrayBuffer()
+    }
 
-//     def variable:Parser[String] = "[a-z]+".r ^^ (s => s)
+    // PRIMS
 
-//     def run_iter(tree: BlockTree): Unit = {
-//         for node <- tree do {
-//             node match {
-//                 case node: BlockNode => {
-//                     node.bt match {
-//                         case BlockType.If => {
-//                             if (LineParser.parseAll(LineParser.line, node.cond).get.v != 0) {
-//                                 run_iter(node.ls)
-//                             }
-//                         }
-//                         case BlockType.While => {
-//                             while (LineParser.parseAll(LineParser.line, node.cond).get.v != 0) {
-//                                 run_iter(node.ls)
-//                             }
-//                         }
-//                         case BlockType.Fn => {
-//                             functions(node.cond) = node.ls
-//                         }
-//                         case util.BlockType.Null => {
-//                             println("wtf")
-//                         }
-//                     }
-//                 }
-//                 case ln: String => {
-//                     LineParser.parseAll(LineParser.line, ln).get
-//                 }
-//             }
-//         }
-//     }
+    def line:Parser[String] = statement ~ ";" ^^ (s => s._1)
 
-//     def run_function(fn_name: String): Wrapper[Any] = {
-//         val tree: BlockTree = functions(fn_name)
-//         val (exec_tree, last_ln) = tree.splitAt(tree.length - 1)
-//         run_iter(exec_tree)
+    def statement:Parser[String] = "[^;{}]*".r ^^ (s => s)
 
-//         last_ln(0) match {
-//             case ln: String => LineParser.parseAll(LineParser.line, ln).get
-//             case default => println("wtf"); Wrapper(0.0)
-//         }
-//     }
+    def variable:Parser[String] = "[a-z]+".r ^^ (s => s)
 
-//     def run(code: String) = {
-//         val min_code = code.replaceAll("\\s", "")
-//                            .replaceAll("if", "if ")
-//                            .replaceAll("while", "while ")
-//                            .replaceAll("fn", "fn ")
-//         val res = parseAll(program, min_code)
-//         run_iter(res.get)
-//     }
+    // RUNNERS
 
-//     def main(args: Array[String]) = {
-//         val code : String = """
-//             x = 10;
-//             while x {
-//                 x -= 1;
-//             }
-//         """
-//         run(code)
-//     }
-// }
+    def run_iter(tree: BlockTree): Unit = {
+        for node <- tree do {
+            node match {
+                case node: BlockNode => {
+                    node.bt match {
+                        case BlockType.If => {
+                            println("cond: " + node.cond)
+                            if (LineParser.parseAll(LineParser.line, node.cond).get != Data.Number(0)) {
+                                run_iter(node.ls)
+                            }
+                        }
+                        case BlockType.While => {
+                            while (LineParser.parseAll(LineParser.line, node.cond).get != Data.Number(0)) {
+                                run_iter(node.ls)
+                            }
+                        }
+                        case BlockType.Fn   => functions(node.cond) = node.ls
+                        case BlockType.Null => println("wtf")
+                    }
+                }
+                case ln: String => {
+                    print("line: " + ln + ". ")
+                    val res = LineParser.parseAll(LineParser.line, ln).get
+                    println(res)
+                }
+            }
+        }
+    }
+
+    def run_function(fn_name: String): Data = {
+        val tree: BlockTree = functions(fn_name)
+        val (exec_tree, last_ln) = tree.splitAt(tree.length - 1)
+        run_iter(exec_tree)
+
+        last_ln(0) match {
+            case ln: String => LineParser.parseAll(LineParser.line, ln).get
+            case default    => println("wtf. missing return type"); Data.Number(0)
+        }
+    }
+
+    def run(code: String) = {
+        val min_code = code.replaceAll("\\s", "")
+                           .replaceAll("if", "if ")
+                           .replaceAll("while", "while ")
+                           .replaceAll("fn", "fn ")
+        val res = parseAll(program, min_code)
+        run_iter(res.get)
+    }
+
+    def main(args: Array[String]) = {
+        // val code : String = """
+        //     x = 10;
+        //     while x {
+        //         x -= 1;
+        //         if x == 5 {
+        //             x -= 1;
+        //         }
+        //     }
+        // """
+
+        val code : String = """
+            x = [];
+            x += 5;
+        """
+        run(code)
+    }
+}
--- util.scala
+++ util.scala
@@ -1,119 +1,89 @@
 package util
 
 import scala.collection.mutable.HashMap
+import java.lang.{String => SString}
+import scala.{Array => AArray}
+import scala.collection.mutable.ArrayBuffer
 
-abstract class Data {
-    case class Number(v: Double) extends Data
-    case class String(v: String) extends Data
-    case class Array(v: List[Data]) extends Data
-    case class Object(v: HashMap[Data, Data]) extends Data
+enum Data {
+    case Number(v: Double) extends Data
+    case String(v: SString) extends Data
+    case Array (v: ArrayBuffer[Data]) extends Data
+    case Object(v: HashMap[Data, Data]) extends Data
 
     def +=(op2: Data): Unit = {
         (this, op2) match {
-            case (Object(v1), Array(v2) ) => v1 += v2(0) -> v2(1)
-            case (String(v1), String(v2)) => v1 += v2
-            case (Number(v1), Number(v2)) => this.Number(v1 + v2)
-            case (Array(v1),  Array(v2) ) => v1 :++ v2
+            case (String(v1), String(v2)) => v1 ++: v2
+            case (Array(v1),  Array(v2) ) => v1.appendAll(v2)
             case (Object(v1), Object(v2)) => v1 ++= v2
+            case (Object(v1), Array(v2) ) => v1 += v2(0) -> v2(1)
+            case (Array(v1),  _)          => v1.append(op2)
+            case (String(v1), Number(v2)) => v1 ++: v2.toString()
+            case default => println("shit!")
         }
     }
 
     def +(op2: Data): Data = {
         (this, op2) match {
-            case (Number(v1), Number(v2)) => new this.Number(v1 + v2)
-            case (String(v1), String(v2)) => new this.String(v1 + v2)
-            case (Array(v1),  Array(v2) ) => new this.Array(v1 ++ v2)
-            case (Object(v1), Object(v2)) => new this.Object(v1 ++ v2)
+            case (Number(v1), Number(v2)) => Data.Number(v1 + v2)
+            case (String(v1), String(v2)) => String(v1 + v2)
+            case (Array(v1),  Array(v2) ) => Array(v1 ++ v2)
+            case (Object(v1), Object(v2)) => Object(v1 ++ v2)
             
-            case (Number(v1), String(v2)) => new this.String(v1.toString() + v2)
-            case (Object(v1), String(v2)) => new this.String(v1.toString() + v2)
-            case (Array(v1), String(v2)) => new this.String(v1.toString() + v2)
-            
-            case (String(v1), Number(v2)) => new this.String(v1 + v2.toString())
-            case (String(v1), Object(v2)) => new this.String(v1 + v2.toString())
-            case (String(v1), Array(v2)) => new this.String(v1 + v2.toString())
+            case (Number(v1), String(v2)) => String(v1.toString() + v2)
+            case (Object(v1), String(v2)) => String(v1.toString() + v2)
+            case (Array(v1),  String(v2)) => String(v1.toString() + v2)
+            case (String(v1), Number(v2)) => String(v1 + v2.toString())
+            case (String(v1), Object(v2)) => String(v1 + v2.toString())
+            case (String(v1), Array(v2))  => String(v1 + v2.toString())
 
-            case default => {
-                println("this aint a + op")
-                new Data.Number(0.0)
-            }
+            case default => println("shit!"); this
         }
     }
 
-    // def -(op2: Data): Data = {
-    //     val v1 = get_tv()
-    //     val v2 = op2.get_tv()
+    def -(op2: Data): Data = {
+        (this, op2) match {
+            case (Number(v1), Number(v2)) => Number(v1 - v2)
+            case default => println("shit!"); this
+        }
+    }
 
-    //     (t, op2.t) match {
-    //         case (NumberType, NumberType)
-    //            | (StringType, StringType)
-    //            | (ObjectType, _) => {
-    //             new Data(v1 - v2)
-    //         }
-    //         case default => {
-    //             println("this aint a + op")
-    //             new Data()
-    //         }
-    //     }
-    // }
+    def *(op2: Data): Data = {
+        (this, op2) match {
+            case (Number(v1), Number(v2)) => Number(v1 * v2)
+            case default => println("shit!"); this
+        }
+    }
 
-    // def ++(): Data = {
-    //     val v1 = get_tv()
+    def /(op2: Data): Data = {
+        (this, op2) match {
+            case (Number(v1), Number(v2)) => Number(v1 / v2)
+            case default => println("shit!"); this
+        }
+    }
 
-    //     t match {
-    //         case NumberType => {
-    //             new Data(v1 + 1)
-    //         }
-    //         case default => {
-    //             println("this aint a + op")
-    //             new Data()
-    //         }
-    //     }
-    // }
+    def ==(op2: Data): Data = {
+        if (this.equals(op2)) Data.Number(1) else Data.Number(0)
+    }
 
-    // def --(): Data = {
-    //     val v1 = get_tv()
+    def !=(op2: Data): Data = {
+        if (this.equals(op2)) Data.Number(0) else Data.Number(1)
+    }
 
-    //     t match {
-    //         case NumberType => {
-    //             new Data(v1 - 1)
-    //         }
-    //         case default => {
-    //             println("this aint a + op")
-    //             new Data()
-    //         }
-    //     }
-    // }
-
-    // def *(op2: Data): Data = {
-    //     val v1 = get_tv()
-    //     val v2 = op2.get_tv()
-
-    //     (t, op2.t) match {
-    //         case (NumberType) => {
-    //             new Data(v1 * v2)
-    //         }
-    //         case default => {
-    //             println("this aint a + op")
-    //             new Data()
-    //         }
-    //     }
-    // }
-
-    // def /(op2: Data): Data = {
-    //     val v1 = get_tv()
-    //     val v2 = op2.get_tv()
-
-    //     (t, op2.t) match {
-    //         case (NumberType) => {
-    //             new Data(v1 / v2)
-    //         }
-    //         case default => {
-    //             println("this aint a + op")
-    //             new Data()
-    //         }
-    //     }
-    // }
+    def equals(op2: Data): Boolean = {
+        (this, op2) match {
+            case (Number(v1), Number(v2)) => v1 == v2
+            case (String(v1), String(v2)) => v1 == v2
+            case (Array(v1), Array(v2))   => {
+                v1.zip(v2).foldLeft(false)((acc, p) => acc || (p._1 == p._2))
+            }
+            case (Object(v1), Object(v2)) => {
+                ((v1.keySet -- v2.keySet).size == 0)
+                    && ((v2.keySet -- v1.keySet).size == 0)
+            }
+            case default => false
+        }
+    }
 }
 
 enum BlockType {
@@ -123,14 +93,22 @@ enum BlockType {
          Null
 }
 
-type BlockTree = List[BlockNode | String]
+type BlockTree = ArrayBuffer[BlockNode | String]
 
 class BlockNode(
     private var _bt: BlockType = BlockType.Null,
     private var _cond: String = "",
-    private var _ls: BlockTree = List()
+    private var _ls: BlockTree = ArrayBuffer()
 ) {
     def bt = _bt
     def cond = _cond
     def ls = _ls
 }
+
+// object Test {
+//     def main(args: AArray[SString]) = {
+//         val v1 = Data.Number(5)
+//         val v2 = Data.Number(4)
+//         print(v1 + v2)
+//     }
+// }
