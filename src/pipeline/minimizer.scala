0d403e066828da7ac3fb11f9eaa99165b10092e6
ArnavMeh <mehraarn000@gmail.com>
2024-01-16 23:47:42 -0500

    cleaned minimizer

--- src/pipeline/minimizer.scala
+++ src/pipeline/minimizer.scala
@@ -1,24 +1,32 @@
 package pipeline.minimizer
 
-def top_keywords = Array("if", "while", "for", "fn")
-def mid_keywords = Array("to", "in")
-def kw_regexes = top_keywords.map(s => (s + "\\s", s + "~"))
-              ++ mid_keywords.map(s => ("\\s" + s + "\\s", "~" + s + "~"))
+import scala.util.chaining._
 
 object Minimizer {
+    def top_keywords = Array("if", "while", "for", "fn")
+    def mid_keywords = Array("to", "in")
+    def kw_regexes = top_keywords.map(s => (s + "\\s", s + "~"))
+                  ++ mid_keywords.map(s => ("\\s" + s + "\\s", "~" + s + "~"))
+
+    def rm_comments(code: String): String = {
+        code.split("\n")
+            .filter(s => s.length() < 2 || s.subSequence(0, 2) != "//")
+            .mkString("\n")
+    }
+
+    def sep_keywords(code: String): String = {
+        kw_regexes.foldLeft(code)((acc, p) => {
+            acc.replaceAll(p._1, p._2)
+        })
+    }
+
+    def rm_whitesp(code: String): String = {
+        code.replaceAll("\\s", "")
+    }
+
     def digest(code: String): String = {
-        // remove comments
-        var mcode: String = (
-            code.split("\n")
-                .filter(s => s.length() < 2 || s.subSequence(0, 2) != "//")
-                .mkString("\n")
-        )
-
-        // clarify keywords
-        kw_regexes.foreach(p => mcode = mcode.replaceAll(p._1, p._2))
-
-        // remove whitespace
-        mcode = mcode.replaceAll("\\s", "")
-        mcode
+        code.pipe(rm_comments)
+            .pipe(sep_keywords)
+            .pipe(rm_whitesp)
     }
 }
